0b2177709fa023ec7579f4f6d0207bb845637f46
Robert <araujorobert302@gmail.com>
2024-07-11 22:51:28 -0300

    primeiro commit

---
+++ python/Aula4.py
@@ -0,0 +1,15 @@
+print ("============= exercício 1 ================")
+nome = input("Digite seu nome: ")
+print("Olá, " + nome + ". Seja bem-vindo(a)")
+
+print("============= exercício 2 ================")
+dia = input("qual o dia do seu nascimento? ")
+mes = input("qual o mês do seu nascimento? ")
+ano = input("qual o ano do seu nascimento? ")
+print ("você nasceu no dia ", dia, " de ", mes, " de ", ano)
+
+
+print ("============= exercício 3 ================")
+num1 = int (input ("insira o primeiro valor: "))
+num2 = int (input("insira o segundo valor: "))
+print("A soma dos valores é: ", num1 + num2)
